debfa3850804ab015fc5e05118d89900f9ea8c92
marinasayols <marinasayols@outlook.com>
2022-06-09 10:33:15 +0200

    fix fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,6 +11,10 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $loader = new NativeLoader();
-        $objectSet = $loader->loadFile(__DIR__ . '/fixtures.yml');
+        $objectSet = $loader->loadFile(__DIR__ . '/fixtures.yml')->getObjects();
+        foreach ($objectSet as $obj) {
+            $manager -> persist($obj);
+        }
+        $manager->flush();
     }
 }
