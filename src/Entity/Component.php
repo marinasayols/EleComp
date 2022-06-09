6e13a489dceb88739e38412e8e0f9112244684e9
marinasayols <marinasayols@outlook.com>
2022-06-09 15:36:55 +0200

    add field NAME to component

--- src/Entity/Component.php
+++ src/Entity/Component.php
@@ -7,6 +7,7 @@ use App\Visitor\Visitor;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints\Unique;
 
 #[ORM\Entity(repositoryClass: ComponentRepository::class)]
 #[ORM\InheritanceType("JOINED")]
@@ -24,6 +25,7 @@ abstract class Component
     private $id;
 
     #[ORM\Column(type: 'string')]
+    #[Unique]
     private $name;
 
     #[ORM\Column(type: 'string', length: 20)]
--- src/Form/ComponentType.php
+++ src/Form/ComponentType.php
@@ -7,9 +7,7 @@ use App\Entity\Manufacturer;
 use App\Entity\Provider;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
-use Symfony\Component\Form\Extension\Core\Type\PercentType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,6 +16,7 @@ class ComponentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('name')
             ->add('value')
             ->add('tolerance')
             ->add('price', MoneyType::class)
