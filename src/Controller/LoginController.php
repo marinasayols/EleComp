63a4e01333e549da122d431c6a46debcf15b346b
marinasayols <marinasayols@outlook.com>
2022-06-19 20:24:40 +0200

    login auto after register

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,10 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Component\Security\Http\Authenticator\FormLoginAuthenticator;
 
 class LoginController extends AbstractController
 {
@@ -20,6 +27,39 @@ class LoginController extends AbstractController
         return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error
+        ]);
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function register(Request                $request, UserPasswordHasherInterface $userPasswordHasher,
+                             EntityManagerInterface $entityManager, UserAuthenticatorInterface $userAuthenticator,
+                             FormLoginAuthenticator $formLoginAuthenticator): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $userAuthenticator->authenticateUser(
+                $user,
+                $formLoginAuthenticator,
+                $request
+            );
+
+            return $this->redirectToRoute('app_component_base');
+        }
+
+        return $this->render('login/register.html.twig', [
+            'registrationForm' => $form->createView(),
         ]);
     }
 }
