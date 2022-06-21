73eec2e0328fbf6832bdfd149d3585e26cecc5b9
marinasayols <marinasayols@outlook.com>
2022-06-21 12:41:57 +0200

    refactor controllers

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -62,4 +62,11 @@ class LoginController extends AbstractController
             'registrationForm' => $form->createView(),
         ]);
     }
+
+    #[Route(path: "/logout", name: "app_logout", methods: ["GET"])]
+    public function logout(): void
+    {
+        // controller can be blank: it will never be called!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
 }
